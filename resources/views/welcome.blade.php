044b5095976076de194ddf9f7850033acc319cc4
banana1982 <loithachcuong@gmail.com>
2017-05-29 11:33:46 +0700

    update views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Demo New
                 </div>
 
                 <div class="links">
